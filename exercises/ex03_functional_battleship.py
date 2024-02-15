80a1bf3f74e1464d6a65e7cf2977071dcc37a63e
Nicholas Hinson <nrhinson@unc.edu>
2024-02-14 22:44:39 -0500

    FinalSumbission4

--- exercises/ex03_functional_battleship.py
+++ exercises/ex03_functional_battleship.py
@@ -6,9 +6,10 @@ __author__ = "730664337"
 
 import random
 
-# Defining main block
-
 def main(grid_size: int, secret_row: int, secret_column: int) -> None:
+
+    """Defining main block"""
+
     turn_count: int = 1
     win_check: bool = False
     while turn_count < 6 and not(win_check):
@@ -26,20 +27,21 @@ def main(grid_size: int, secret_row: int, secret_column: int) -> None:
         turn_count += 1
     if not(correct_check):
         print("X/5 - Better luck next time!")
-    
-
-# Defining the function that asks the player for their imput and checks if its vaild
 
 def input_guess(board_size: int, type_of_guess: str) -> int:
+
+    """This is the function that asks the player for their imput and checks if its vaild"""
+
     assert type_of_guess == "row" or type_of_guess == "column"
     player_input: int = int(input(f"Guess a {type_of_guess}: "))
     while (player_input < 1 or player_input > board_size):
         player_input = int(input(f"The grid is only {board_size} by {board_size}. Try again: "))
     return player_input
 
-# Defining th function that prints the battleship board once given a guess
-
 def print_grid(board_size: int, row_guess: int, column_guess: int, correct_check: bool) -> None:
+
+    "This is the function that prints the battleship board once given a guess"
+
     # Initalize variables
     BLUE_BOX: str = "\U0001F7E6"
     RED_BOX: str = "\U0001F7E5"
@@ -63,9 +65,10 @@ def print_grid(board_size: int, row_guess: int, column_guess: int, correct_check
         column_counter = 1
         row_counter += 1
 
-# Defining the function that checks to see if the player hit the correct spot
-
 def correct_guess(secret_row: int, secret_column: int, guessed_row: int, guessed_column: int) -> bool:
+
+    """This is the function that checks to see if the player hit the correct spot"""
+
     if(secret_row == guessed_row and secret_column == guessed_column):
         return True
     return False
