e0a59ae78c6c8aa81c7222ac493fb03474f44b14
Nicholas Hinson <nrhinson@unc.edu>
2024-02-19 21:48:31 -0500

    Final3Mbby

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,9 +1,10 @@
-"""EX04"""
+"""EX04."""
 
 __author__ = "730664337"
 
+
 def all(listA: list[int], givenInt: int) -> bool:
-    """This function is meant to check to see if the give int is the only int in the list"""
+    """This function is meant to check to see if the give int is the only int in the list."""
     index: int = 0
     if len(listA) == 0:
         return False
@@ -15,7 +16,7 @@ def all(listA: list[int], givenInt: int) -> bool:
 
 
 def max(listB: list[int]) -> int:
-    """This function is meant to find the largest number in a list"""
+    """This function is meant to find the largest number in a list."""
     index: int = 1
     highestNum: int = listB[0]
     while index < len(listB):
@@ -26,18 +27,19 @@ def max(listB: list[int]) -> int:
 
 
 def is_equal(listC: list[int], listD: list[int]) -> bool:
-    """This function is meant to find if two lists are identical to each other"""
+    """This function is meant to find if two lists are identical to each other."""
     index: int = 0
     if len(listC) != len(listD):
         return False
     while index < len(listC):
         if listC[index] != listD[index]:
             return False
+        index += 1
     return True
 
 
 def extend(baseList: list[int], addList: list[int]) -> None:
-    """This functions is meant to add a list to another list"""
+    """This functions is meant to add a list to another list."""
     index: int = 0
     while index < len(addList):
         baseList.append(addList[index])
