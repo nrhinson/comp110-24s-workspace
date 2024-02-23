16e1e665960da9f284fdd731743ca1c3acd7312c
Nicholas Hinson <nrhinson@unc.edu>
2024-02-23 18:40:54 -0500

    LessonChallenge3SaveFinal

---
+++ lessons/sum.py
@@ -0,0 +1,30 @@
+"""Summing the elements of a list using different loops."""
+
+__author__ = "730664337"
+
+
+def w_sum(vals: list[float]) -> float:
+    """This function uses a while loop to add all floats in a list."""
+    total: float = 0.0
+    listIndex: int = 0
+    while listIndex < len(vals):
+        total += vals[listIndex]
+        listIndex += 1
+    return total
+
+
+def f_sum(vals: list[float]) -> float:
+    """This function uses a for loop to add all floats in a list."""
+    total: float = 0.0
+    for index in vals:
+        total += index
+    return total
+
+
+def f_range_sum(vals: list[float]) -> float:
+    """This function uses a for - in - range loop to add all floats in a list."""
+    total: float = 0.0
+    for index in range(0, len(vals)):
+        total += vals[index]
+    return total
+
