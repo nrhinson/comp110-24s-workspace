0ed99fbf38a17b80a7deb316cb7f8d87505eb35a
Nicholas Hinson <nrhinson@unc.edu>
2024-02-28 17:08:39 -0500

    Unzipsave3

--- lessons/unzip.py
+++ lessons/unzip.py
@@ -3,17 +3,17 @@
 __author__ = "730664337"
 
 
-def get_keys (givenDict: dict[str, int]) -> list:
+def get_keys (givenDict: dict[str, int]) -> list[str]:
     """This function returns all the keys in a dictonary."""
-    returnList: list = []
+    returnList: list = [str]
     for i in givenDict:
         returnList.append(i)
     return returnList
 
 
-def get_values (givenDict: dict[str, int]) -> list:
+def get_values (givenDict: dict[str, int]) -> list[int]:
     """This function returns all the values in a dictonary."""
-    returnList: list = []
+    returnList: list = [int]
     for i in givenDict:
         returnList.append(givenDict[i])
     return returnList
