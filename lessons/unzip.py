7c0a84a543cb944d724157bd1e6bc89d715d8274
Nicholas Hinson <nrhinson@unc.edu>
2024-02-28 16:58:07 -0500

    Unzipsave2

--- lessons/unzip.py
+++ lessons/unzip.py
@@ -1,19 +1,19 @@
-"""Splitting a dictionary into two lists"""
+"""Splitting a dictionary into two lists."""
 
 __author__ = "730664337"
 
 
-def get_keys (givenDict: dict[str, int]) -> None:
-    """This function returns all the keys in a dictonary"""
-    returnList = []
+def get_keys (givenDict: dict[str, int]) -> list:
+    """This function returns all the keys in a dictonary."""
+    returnList: list = []
     for i in givenDict:
         returnList.append(i)
     return returnList
 
 
-def get_values(givenDict: dict[str, int]) -> None:
-    """This function returns all the values in a dictonary"""
-    returnList = []
+def get_values (givenDict: dict[str, int]) -> list:
+    """This function returns all the values in a dictonary."""
+    returnList: list = []
     for i in givenDict:
         returnList.append(givenDict[i])
     return returnList
