e4e6d84c2c87d0ae5713022af136f1cc947376d5
Nicholas Hinson <nrhinson@unc.edu>
2024-03-28 21:53:00 -0400

    update1

--- exercises/ex05/dictionary.py
+++ exercises/ex05/dictionary.py
@@ -3,78 +3,78 @@
 __author__ = "730664337"
 
 
-def invert(givenDict: dict[str, str]) -> dict[str, str]:
+def invert(given_dict: dict[str, str]) -> dict[str, str]:
     """This function inverts the keys and indexs of a dictionary."""
-    returnDict: dict[str, str] = {}
-    for i in givenDict:
-        returnDict[givenDict[i]] = i
-    return returnDict
+    return_dict: dict[str, str] = {}
+    for i in given_dict:
+        return_dict[given_dict[i]] = i
+    return return_dict
 
 
-def favorite_color(givenDict: dict[str, str]) -> str:
+def favorite_color(given_dict: dict[str, str]) -> str:
     """This function counts the most common colors out of a list of people and their favorte color."""
-    mostCommonColor: str
-    mostCommonColorCount: int = 0
+    most_common_color: str
+    most_common_colorCount: int = 0
     counter: int
-    for i in givenDict:
+    for i in given_dict:
         counter = 0
-        for b in givenDict:
-            if givenDict[i] == givenDict[b]:
+        for b in given_dict:
+            if given_dict[i] == given_dict[b]:
                 counter += 1
-        if counter > mostCommonColorCount:
-            mostCommonColor = givenDict[i]
-            mostCommonColorCount = counter
-    return mostCommonColor
+        if counter > most_common_colorCount:
+            most_common_color = given_dict[i]
+            most_common_colorCount = counter
+    return most_common_color
 
 
 def count(givenList: list[str]) -> dict[str, int]:
     """This function counts how much of each string is in a list."""
-    howMuch: int = 0
-    returnDict: dict[str, int] = {}
+    how_much: int = 0
+    return_dict: dict[str, int] = {}
     for a in givenList:
-        alreadyRan: bool = False
-        for b in returnDict:
+        already_ran: bool = False
+        for b in return_dict:
             if b == a:
-                alreadyRan = True
-        if not alreadyRan:
-            howMuch = 0
+                already_ran = True
+        if not already_ran:
+            how_much = 0
             for c in givenList:
                 if c == a:
-                    howMuch += 1
-            returnDict[a] = howMuch
-    return returnDict
+                    how_much += 1
+            return_dict[a] = how_much
+    return return_dict
 
 
 def alphabetizer(givenList: list[str]) -> dict[str, list[str]]:
     """This function sorts words in a list by the first letter of their string."""
-    returnDict: dict[str, list[str]] = {}
-    buildList: list[str] = []
+    return_dict: dict[str, list[str]] = {}
+    build_list: list[str] = []
     for a in givenList:
         a = a[0].lower()
-        buildList = []
-        alreadyRan: bool = False
-        for b in returnDict:
+        build_list = []
+        already_ran: bool = False
+        for b in return_dict:
             b = b[0].lower()
             if a == b:
-                alreadyRan = True
-        if not alreadyRan:
+                already_ran = True
+        if not already_ran:
             for c in givenList:
                 if c[0].lower() == a:
-                    buildList.append(c)
-            returnDict[a] = buildList
-    return returnDict
+                    build_list.append(c)
+            return_dict[a] = build_list
+    return return_dict
 
 
-def update_attendance(givenDict: dict[str, list[str]], day: str, student: str) -> None:
+def update_attendance(given_dict: dict[str, list[str]], day: str, student: str) -> None:
     """This function is used to update attendance."""
-    keyAlreadyUsed: bool = False
-    newList: list[str]
-    for i in givenDict:
+    key_already_used: bool = False
+    new_list: list[str]
+    for i in given_dict:
         if day == i:
-            keyAlreadyUsed = True
-    if keyAlreadyUsed:
-        newList = givenDict[day]
-        newList.append(student)
-        givenDict[day] = newList
+            key_already_used = True
+    if key_already_used:
+        new_list = given_dict[day]
+        new_list.append(student)
+        given_dict[day] = new_list
     else:
-        givenDict[day] = [student]
+        given_dict[day] = [student]
---
+++ exercises/ex05/dictionary_test.py
@@ -0,0 +1,111 @@
+"""EX05 - Dictionary Utils."""
+
+__author__ = "730664337"
+
+from dictionary import *
+import pytest
+
+def test_invert_use1 () -> None:
+    """This tests the invert function once."""
+    new_dict: dict[str, str] = {"key": "one", "anotherKey": "two"}
+    changed_dict: dict[str, str] = invert(new_dict)
+    assert changed_dict == {"one": "key", "two": "anotherKey"}
+
+
+def test_invert_use2 () -> None:
+    """this tests to make sure inverting the function twice returns it to its orignal value."""
+    new_dict: dict[str, str] = {"key": "one", "anotherKey": "two"}
+    changed_dict: dict[str, str] = invert(new_dict)
+    changed_again_dict: dict[str, str] = invert(changed_dict)
+    assert changed_again_dict == {"key": "one", "anotherKey": "two"}
+
+
+def test_invert_edge () -> None:
+    """This tests what happens when a int is used instead of a str."""
+    new_dict: dict[str, int] = {"key": 1, "anotherKey": 2}
+    changed_dict: dict[int, str] = invert(new_dict)
+    assert changed_dict == {1: "key", 2: "anotherKey"}
+
+
+def test_favorite_color_use1 () -> None:
+    """This checks to make sure the most common color us pulled from the dictonary."""
+    new_dict: dict[str, str] = {"Nick": "Green", "Joe": "Yellow", "Jackson": "Green", "Josh": "Blue"}
+    new_str: str = favorite_color(new_dict)
+    assert new_str == "Green"
+
+
+def test_favorite_color_use2 () -> None:
+    """This checks what happens if there is only one of each color."""
+    new_dict: dict[str, str] = {"Nick": "Green", "Joe": "Yellow", "Jackson": "Orange", "Josh": "Blue"}
+    new_str: str = favorite_color(new_dict)
+    assert new_str == "Green"
+
+
+def test_favorite_color_edge () -> None:
+    """This checks what happens if the dictionary does not contain colors."""
+    new_dict: dict[str, str] = {"Nick": "Apple", "Joe": "Watermelon", "Jackson": "Grapes", "Josh": "Oranges"}
+    new_str: str = favorite_color(new_dict)
+    assert new_str == "Apple"
+
+
+def test_count_use1 () -> None:
+    """This checks what happens when the def "count" is used as intended"""
+    new_list: list[str] = ["Hi", "Hello", "Hi", "Hi", "Hello", "Hey"]
+    new_dict: dict[str,str] = count(new_list)
+    assert new_dict == {"Hi": 3, "Hello": 2, "Hey": 1}
+    
+
+def test_count_use2 () -> None:
+    """This checks what happens when the list given is all one word"""
+    new_list: list[str] = ["Hey", "Hey", "Hey", "Hey", "Hey", "Hey", "Hey"]
+    new_dict: dict[str,str] = count(new_list)
+    assert new_dict == {"Hey": 7}
+
+
+def test_count_edge () -> None:
+    """This checks what happens when an empty list is given"""
+    new_list: list[str] = []
+    new_dict: dict[str,str] = count(new_list)
+    assert new_dict == {}
+
+
+def test_alphabetizer_use1 () -> None:
+    """This tests what happens when words are givin"""
+    new_list: list[str] = ["Cat", "Dog", "Parrot", "Snake", "Turtle"]
+    new_dict: dict[str, list[str]] = alphabetizer(new_list)
+    assert new_dict == {"c": ["Cat"], "d": ["Dog"], "p": ["Parrot"], "s": ["Snake"], "t": ["Turtle"]}
+
+
+def test_alphabetizer_use2 () -> None:
+    """This tests what happens when a mix of lowercase and captial words are given"""
+    new_list: list[str] = ["cat", "Dog", "parrot", "Snake", "Turtle"]
+    new_dict: dict[str, list[str]] = alphabetizer(new_list)
+    assert new_dict == {"c": ["cat"], "d": ["Dog"], "p": ["parrot"], "s": ["Snake"], "t": ["Turtle"]}
+
+
+def test_alphabetizer_edge () -> None:
+    """This tests what happens when numbers are given"""
+    new_list: list[str] = ["1 cat", "2 Dog", "3 Parrot", "4 Snake", "5 Turtle"]
+    new_dict: dict[str, list[str]] = alphabetizer(new_list)
+    assert new_dict == {"1": ["1 cat"], "2": ["2 Dog"], "3": ["3 Parrot"], "4": ["4 Snake"], "5": ["5 Turtle"]}
+
+    
+
+def update_attendance_use1 () -> None:
+    """This tests adding a student to an already existing day"""
+    new_list: dict[str, list[str]] = {"Monday": ["Joe", "Jackson", "Nick"], "Tuesday": ["Nick", "Joe"], "Wendsay": ["Jackson", "Josh", "Alex"]}
+    update_attendance(new_list, "Tuesday", "Alex")
+    assert new_list == {"Monday": ["Joe", "Jackson", "Nick"], "Tuesday": ["Nick", "Joe", "Alex"], "Wendsay": ["Jackson", "Josh", "Alex"]}
+
+def update_attendance_use2 () -> None:
+    """This test adds a studnet to a day that does not exist"""
+    new_list: dict[str, list[str]] = {"Monday": ["Joe", "Jackson", "Nick"], "Tuesday": ["Nick", "Joe"], "Wendsay": ["Jackson", "Josh", "Alex"]}
+    update_attendance(new_list, "Tuesday", "Alex")
+    assert new_list == {"Monday": ["Joe", "Jackson", "Nick"], "Wendsay": ["Jackson", "Josh", "Alex"]}
+    
+
+def update_attendance_edge () -> None:
+    """This test test what happens when you add a name that already exists on said day"""
+    new_list: dict[str, list[str]] = {"Monday": ["Joe", "Jackson", "Nick"], "Tuesday": ["Nick", "Joe", "Alex"], "Wendsay": ["Jackson", "Josh", "Alex"]}
+    update_attendance(new_list, "Monday", "Joe")
+    assert new_list == {"Monday": ["Joe", "Jackson", "Nick", "Joe"], "Tuesday": ["Nick", "Joe", "Alex"], "Wendsay": ["Jackson", "Josh", "Alex"]}
