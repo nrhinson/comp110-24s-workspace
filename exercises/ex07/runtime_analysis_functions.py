eae17a05974e5014d6160653043ffcb5a8bdb1c7
Nicholas Hinson <nrhinson@unc.edu>
2024-04-02 23:14:41 -0400

    save2

--- exercises/ex07/runtime_analysis_functions.py
+++ exercises/ex07/runtime_analysis_functions.py
@@ -8,7 +8,7 @@ MAX_VAL: int = 10 ** 5
 def random_descending_list(n: int) -> list[int]:
     """Generate a list of random descending integers."""
     new_list: list[int] = []
-    for num in range(0, n):
+    for num in range(0, n-1):
         new_list.append(random.randint(0-MAX_VAL, MAX_VAL))
     current_index: int = 1
     location_of_num_to_insert: int = 0
@@ -21,6 +21,9 @@ def random_descending_list(n: int) -> list[int]:
                 location_of_num_to_insert -= 1
         current_index += 1
     return new_list
+
+mylist = random_descending_list(37)
+print(len(mylist))
 
 def evaluate_runtime(fn_name, start_size: int, end_size: int) -> np.array:
     """Evaluate the runtime for different size inputs."""
