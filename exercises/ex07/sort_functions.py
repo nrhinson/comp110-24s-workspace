61a28e75a102dc27930baf8f5736543698d210eb
Nicholas Hinson <nrhinson@unc.edu>
2024-04-02 21:06:47 -0400

    Sync1

--- exercises/ex07/sort_functions.py
+++ exercises/ex07/sort_functions.py
@@ -2,14 +2,44 @@
 
 __author__ = ""
 
+
 def insertion_sort(x: list[int]) -> None:
-    """Basic insertion sort algorithm.
-    Insert into an already sorted list."""
+    stored_int: int = 0
+    num_of_sorted_int: int = 0
+    for index in x:
+        currentInt: int = index
+        for potental_greater in x:
+            print("2")
+            if currentInt > second_index:  
+                print("3")
+                stored_int = x[index]
+                x[index] = currentInt
+                x[second_index] = stored_int
+                currentInt = x[index]
+        num_of_sorted_int =+ 1
     return None
 
 
 def selection_sort(x: list[int]) -> None:
-    """Basic selection sort algorithm. 
-    Repeatedly find the minimum and swap it."""
+    index_of_current: int = 0
+    current_min: int = 0
+    index_of_min: int = 0
+    stored_index: int = 0
+    swap_number: int = 0
+    for selcted_index in x:
+        current_min = selcted_index
+        index_of_min = index_of_current
+        for check_min in x[index_of_current:]:
+            if current_min > check_min:
+                current_min = check_min
+                stored_index = index_of_min
+            index_of_min += 1
+        swap_number = x[stored_index]
+        x[stored_index] = x[index_of_current]
+        x[index_of_current] = swap_number
+        index_of_current += 1
     return None
-    
+
+list1: list[int] = [2, 4, 3, 6]
+selection_sort(list1)
+print(list1)
