a216e26a96bd33f45f7e781802debb21d5b9bfff
Nicholas Hinson <nrhinson@unc.edu>
2024-04-05 20:19:16 -0400

    Save4

--- lessons/recursion.py
+++ lessons/recursion.py
@@ -1,5 +1,12 @@
+"""CQ07."""
+
+__author__ = "730664337"
+
+
 def f(n: int, k: int) -> int:
+    """Recursion fucntion that is used to multipy."""
     if n == 0:
         return 0
     else:
         return k + f(n-1, k)
+
